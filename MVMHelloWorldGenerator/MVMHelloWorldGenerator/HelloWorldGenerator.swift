580aacf6776b616848639fa721c837ec995ff185
Vladimir Mironiuk <vladimironiuk@gmail.com>
2021-03-11 20:15:21 +0200

    Add framework

---
+++ MVMHelloWorldGenerator/MVMHelloWorldGenerator/HelloWorldGenerator.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldGenerator.swift
+//  MVMHelloWorldGenerator
+//
+//  Created by Vladimir Mironiuk on 11.03.2021.
+//
+
+import Foundation
+
+public class HelloWorldGenerator {
+    
+    private init() {}
+    
+    public static func generateHelloWorld() -> String {
+        "Hello, world!"
+    }
+}
---
+++ MVMHelloWorldGenerator/MVMHelloWorldGenerator/MVMHelloWorldGenerator.h
@@ -0,0 +1,18 @@
+//
+//  MVMHelloWorldGenerator.h
+//  MVMHelloWorldGenerator
+//
+//  Created by Vladimir Mironiuk on 11.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MVMHelloWorldGenerator.
+FOUNDATION_EXPORT double MVMHelloWorldGeneratorVersionNumber;
+
+//! Project version string for MVMHelloWorldGenerator.
+FOUNDATION_EXPORT const unsigned char MVMHelloWorldGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVMHelloWorldGenerator/PublicHeader.h>
+
+
